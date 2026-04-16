7256149221c3120a09c54aa5d9ef57a5f99f6e58
RamoLAB <britods.guilherme@gmail.com>
2026-04-16 15:43:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,17 +3,25 @@ const app = express();
 
 app.use(express.json());
 
+// libera acesso entre domínios (CORS)
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
 app.get('/', (req, res) => {
-  res.send('Servidor online funcionando');
+    res.send('Servidor online funcionando');
 });
 
 app.post('/posicao', (req, res) => {
-  console.log(req.body);
-  res.send('ok');
+    console.log(req.body);
+    res.send('ok');
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log('Servidor rodando na porta ' + PORT);
+    console.log('Servidor rodando na porta ' + PORT);
 });
