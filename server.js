52068f3b5fe32253dd0d70f76690be32b6e7f581
RamoLAB <britods.guilherme@gmail.com>
2026-04-16 16:46:38 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,13 +8,25 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type");
-
     if (req.method === "OPTIONS") return res.sendStatus(200);
     next();
 });
 
-// armazenamento simples de jogadores
 const players = {};
+
+// tempo de vida (ms)
+const TIMEOUT = 5000;
+
+// cleanup automático
+setInterval(() => {
+    const now = Date.now();
+
+    for (let id in players) {
+        if (now - players[id].last > TIMEOUT) {
+            delete players[id];
+        }
+    }
+}, 2000);
 
 app.get('/players', (req, res) => {
     res.json(players);
@@ -22,18 +34,18 @@ app.get('/players', (req, res) => {
 
 app.post('/posicao', (req, res) => {
     const { id, x, y } = req.body;
+    if (!id) return res.send("no id");
 
-    if (!id) return res.send("sem id");
-
-    players[id] = { x, y };
-
-    console.log("PLAYERS:", players);
+    players[id] = {
+        x,
+        y,
+        last: Date.now()
+    };
 
     res.send("ok");
 });
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, '0.0.0.0', () => {
-    console.log('Servidor rodando na porta ' + PORT);
+    console.log("server on " + PORT);
 });
